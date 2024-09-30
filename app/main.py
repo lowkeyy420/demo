eaab29c2814cd3a28cad95cb5147512290ee9da9
Josafat Anderson <89123059+lowkeyy420@users.noreply.github.com>
2024-09-30 16:26:12 +0700

    feat: add question mark

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def greeting(name):
-    greeting_prefix = os.getenv('GREETING_PREFIX', 'Hello')
+    greeting_prefix = s.getenv('GREETING_PREFIX', 'Hello')
     return f"{greeting_prefix}, {name}!"
 
 if __name__ == "__main__":
     print(greeting("World"))
     print(greeting("What"))
+    print(greeting("???"))
